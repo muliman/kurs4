39882a0722db63e896a91f48d1a0c7999c873b40
muliman <muliman.ru>
2022-10-13 17:36:55 +0300

    add new functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
-import tensorflow
-import tensorflow.keras
+import tensorflow as tf
 from absl import logging
 from itertools import repeat
 
@@ -9,17 +8,99 @@ yolo_iou_threshold = 0.6  # порог пересечения относител
 yolo_score_threshold = 0.6
 weights_yolov3 = '~/Desktop/kurs4/checkpoints/yolov3.weights'  # путь к файлу весов
 weights = '~/Desktop/kurs4/checkpoints/yolov3.tf'  # путь к файлу checkpoint'ов
-size = 416             # приводим изображения к этому размеру
+size = 416  # приводим изображения к этому размеру
 checkpoints = '~/Desktop/kurs4/checkpoints/yolov3.tf'
-num_classes = 80   # количество классов в модели
+num_classes = 80  # количество классов в модели
 
 YOLO_V3_LAYERS = [
-  'yolo_darknet',
-  'yolo_conv_0',
-  'yolo_output_0',
-  'yolo_conv_1',
-  'yolo_output_1',
-  'yolo_conv_2',
-  'yolo_output_2',
+    'yolo_darknet',
+    'yolo_conv_0',
+    'yolo_output_0',
+    'yolo_conv_1',
+    'yolo_output_1',
+    'yolo_conv_2',
+    'yolo_output_2',
 ]
+
+
+def load_darknet_weights(model, weights_file):
+    wf = open(weights_file, 'rb')
+    major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
+    layers = YOLO_V3_LAYERS
+
+    for layer_name in layers:
+        sub_model = model.get_layer(layer_name)
+        for i, layer in enumerate(sub_model.layers):
+            if not layer.name.startswith('conv2d'):
+                continue
+            batch_norm = None
+            if i + 1 < len(sub_model.layers) and \
+                    sub_model.layers[i + 1].name.startswith('batch_norm'):
+                batch_norm = sub_model.layers[i + 1]
+
+            logging.info("{}/{} {}".format(
+                sub_model.name, layer.name, 'bn' if batch_norm else 'bias'))
+
+            filters = layer.filters
+            size = layer.kernel_size[0]
+            in_dim = layer.input_shape[-1]
+
+            if batch_norm is None:
+                conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
+            else:
+                bn_weights = np.fromfile(
+                    wf, dtype=np.float32, count=4 * filters)
+
+                bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
+
+            conv_shape = (filters, in_dim, size, size)
+            conv_weights = np.fromfile(
+                wf, dtype=np.float32, count=np.product(conv_shape))
+
+            conv_weights = conv_weights.reshape(
+                conv_shape).transpose([2, 3, 1, 0])
+
+            if batch_norm is None:
+                layer.set_weights([conv_weights, conv_bias])
+            else:
+                layer.set_weights([conv_weights])
+                batch_norm.set_weights(bn_weights)
+
+    assert len(wf.read()) == 0, 'не удалось прочитать все данные'
+    wf.close()
+
+
+def interval_overlap(interval_1, interval_2):
+    x1, x2 = interval_1
+    x3, x4 = interval_2
+    if x3 < x1:
+        return 0 if x4 < x1 else (min(x2, x4) - x1)
+    else:
+        return 0 if x2 < x3 else (min(x2, x4) - x3)
+
+
+def intersection_over_union(box1, box2):
+    intersect_w = interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+    intersect_h = interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])
+    intersect_area = intersect_w * intersect_h
+
+    w1, h1 = box1.xmax - box1.xmin, box1.ymax - box1.ymin
+    w2, h2 = box2.xmax - box2.xmin, box2.ymax - box2.ymin
+
+    union_area = w1 * h1 + w2 * h2 - intersect_area
+    return float(intersect_area) / union_area
+
+
+def draw_outputs(img, outputs, class_names):
+    boxes, score, classes, nums = outputs
+    boxes, score, classes, nums = boxes[0], score[0], classes[0], nums[0]
+    wh = np.flip(img.shape[0:2])
+    for i in range(nums):
+        x1y1 = tuple((np.array(boxes[i][0:2]) * wh).astype(np.int32))
+        x2y2 = tuple((np.array(boxes[i][2:4]) * wh).astype(np.int32))
+        img = cv2.rectangle(img, x1y1, x2y2, (255, 0, 0), 2)
+        img = cv2.putText(img, '{} {:.4f}'.format(
+            class_names[int(classes[i])], score[i]),
+                          x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+    return img
 
