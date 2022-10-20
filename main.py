80da26c67270852927882ee6312f96ce85aaeb02
muliman <newscamander72@gmail.com>
2022-10-21 00:08:24 +0300

    add new file, and update main

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import cv2
 import tensorflow as tf
 from absl import logging
 from itertools import repeat
+import yolov3
+
 
 yolo_iou_threshold = 0.6  # порог пересечения относительно объединения (iou)
 yolo_score_threshold = 0.6
-weights_yolov3 = '~/Desktop/kurs4/checkpoints/yolov3.weights'  # путь к файлу весов
+weight_yolov3 = '~/Desktop/kurs4/checkpoints/yolov3.weights'  # путь к файлу весов
 weights = '~/Desktop/kurs4/checkpoints/yolov3.tf'  # путь к файлу checkpoint'ов
 size = 416  # приводим изображения к этому размеру
 checkpoints = '~/Desktop/kurs4/checkpoints/yolov3.tf'
@@ -104,3 +106,106 @@ def draw_outputs(img, outputs, class_names):
                           x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
     return img
 
+@tf.function
+def transform_targets_for_output(y_true, grid_size, anchor_idxs, classes):
+
+    N = tf.shape(y_true)[0]
+
+    y_true_out = tf.zeros(
+      (N, grid_size, grid_size, tf.shape(anchor_idxs)[0], 6))
+
+    anchor_idxs = tf.cast(anchor_idxs, tf.int32)
+
+    indexes = tf.TensorArray(tf.int32, 1, dynamic_size=True)
+    updates = tf.TensorArray(tf.float32, 1, dynamic_size=True)
+    idx = 0
+    for i in tf.range(N):
+        for j in tf.range(tf.shape(y_true)[1]):
+            if tf.equal(y_true[i][j][2], 0):
+                continue
+            anchor_eq = tf.equal(
+                anchor_idxs, tf.cast(y_true[i][j][5], tf.int32))
+
+            if tf.reduce_any(anchor_eq):
+                box = y_true[i][j][0:4]
+                box_xy = (y_true[i][j][0:2] + y_true[i][j][2:4]) / 2
+
+                anchor_idx = tf.cast(tf.where(anchor_eq), tf.int32)
+                grid_xy = tf.cast(box_xy // (1/grid_size), tf.int32)
+
+                indexes = indexes.write(
+                    idx, [i, grid_xy[1], grid_xy[0], anchor_idx[0][0]])
+                updates = updates.write(
+                    idx, [box[0], box[1], box[2], box[3], 1, y_true[i][j][4]])
+                idx += 1
+
+    return tf.tensor_scatter_nd_update(
+        y_true_out, indexes.stack(), updates.stack())
+
+
+def transform_targets(y_train, anchors, anchor_masks, classes):
+    outputs = []
+    grid_size = 13
+
+    anchors = tf.cast(anchors, tf.float32)
+    anchor_area = anchors[..., 0] * anchors[..., 1]
+    box_wh = y_train[..., 2:4] - y_train[..., 0:2]
+    box_wh = tf.tile(tf.expand_dims(box_wh, -2),
+                    (1, 1, tf.shape(anchors)[0], 1))
+    box_area = box_wh[..., 0] * box_wh[..., 1]
+    intersection = tf.minimum(box_wh[..., 0], anchors[..., 0]) * \
+    tf.minimum(box_wh[..., 1], anchors[..., 1])
+    iou = intersection / (box_area + anchor_area - intersection)
+    anchor_idx = tf.cast(tf.argmax(iou, axis=-1), tf.float32)
+    anchor_idx = tf.expand_dims(anchor_idx, axis=-1)
+
+    y_train = tf.concat([y_train, anchor_idx], axis=-1)
+
+    for anchor_idxs in anchor_masks:
+        outputs.append(transform_targets_for_output(
+            y_train, grid_size, anchor_idxs, classes))
+        grid_size *= 2
+
+    return tuple(outputs) # [x, y, w, h, obj, class]
+
+
+def preprocess_image(x_train, size):
+    return (tf.image.resize(x_train, (size, size))) / 255
+
+
+yolo = yolov3.YoloV3(classes=num_classes)
+
+load_darknet_weights(yolo, weight_yolov3)
+
+yolo.save_weights(checkpoints)
+
+class_names = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+               "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+               "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+               "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+               "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+               "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl",
+               "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut",
+               "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop",
+               "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink",
+               "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+
+def detect_objects(img_path, white_list=None):
+    image = img_path  # Путь к изображению.
+    img = tf.image.decode_image(open(image, 'rb').read(), channels=3)
+
+    img = tf.expand_dims(img, 0)
+    img = preprocess_image(img, size)
+    boxes, scores, classes, nums = yolo(img)
+
+    img = cv2.imread(image)
+    img = draw_outputs(img, (boxes, scores, classes, nums), class_names, white_list)
+
+    cv2.imwrite('detected_{:}'.format(img_path), img)
+
+    detected = Image.open('detected_{:}'.format(img_path))
+    detected.show()
+
+
+detect_objects('test.jpg', ['bear'])
